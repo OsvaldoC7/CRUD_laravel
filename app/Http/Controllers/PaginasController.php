68b04e1088e4e9c9f6710ec1abac67b3e4372906
OsvaldoC7 <osva.6cruz@gmail.com>
2021-09-25 21:41:34 -0500

    Conexion a base de datos con modelo

--- app/Http/Controllers/PaginasController.php
+++ app/Http/Controllers/PaginasController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contacto;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -23,13 +24,25 @@ class PaginasController extends Controller {
     
     public function recibeContacto(Request $request) {
 
-        DB::table('contactos')->insert([
-            'correo' => $request->correo,
-            'telefono' => $request->telefono,
-            'comentario' => $request->comentario,
-            'created_at' => now(),
-            'updated_at' => now()
+        $request->validate([
+            'correo' => 'required|email',
+            'comentario' => ['required', 'min:5', 'max:500'],
+            'telefono' => 'numeric'
         ]);
+
+        $contacto = new Contacto();
+        $contacto->correo = $request->correo;
+        $contacto->telefono = $request->telefono;
+        $contacto->comentario = $request->comentario;
+        $contacto->save();
+
+        // DB::table('contactos')->insert([
+        //     'correo' => $request->correo,
+        //     'telefono' => $request->telefono,
+        //     'comentario' => $request->comentario,
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
 
         return redirect()->route('contacto');
 
--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -15,17 +15,27 @@
         <li><a href="{{route('contacto')}}">Contacto</a></li>
     </ul>
 
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="{{route('recibe_contacto')}}" method="POST">
 
         @csrf
         <label for="correo">Correo: </label><br>
-        <input type="email" name="correo" id="correo">
+        <input type="email" name="correo" id="correo" required>
         <br>
         <label for="telefono">Telefono: </label><br>
         <input type="text" name="telefono" id="telefono">
         <br>
         <label for="comentario">Comentario: </label><br>
-        <textarea name="comentario" id="comentario" cols="30" rows="5"></textarea>
+        <textarea name="comentario" id="comentario" cols="30" rows="5" required></textarea>
         <br>
         <input type="submit" value="Enviar">
 
